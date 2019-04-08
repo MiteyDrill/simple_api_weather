0d69e871d623348e94c4d95f5a449d54674527a7
Benjamin Sanchez <Linkandcheezer@gmail.com>
2019-04-08 12:43:15 -0700

    added geolocation position.coords

--- script.js
+++ script.js
@@ -2,21 +2,53 @@
 
 let request = new XMLHttpRequest ();
 
-request.open('GET', " https://api.darksky.net/forecast/1778cc0d0f62e65c0b1e805adab619f5/40.7127,-74.0059", true);
+//get location
 
-request.onload = function () {
-  //access JSON data here
+if ("geolocation" in navigator){
 
-  let data = JSON.parse(this.response);
+  navigator.geolocation.getCurrentPosition(function(position) {
 
-  console.log(data);
 
-  let temp = 
+  request.open('GET', " https://api.darksky.net/forecast/1778cc0d0f62e65c0b1e805adab619f5/" + position.coords.latitude + "," + position.coords.longitude  + "", true);
+
+
+  request.onload = function () {
+    //access JSON data here
+
+    let data = JSON.parse(this.response);
+
+    console.log(data);
+
+    //set/grab default variables
+    let temp = -1;
+    let summ = "default description";
+    let wind_speed = -1;
+    let rain_chance = -1;
+    let icon = "clear_day";
+
+    temp = data.currently.apparentTemperature;
+    summ = data.currently.summary;
+    wind_speed = data.currently.windSpeed;
+    rain_chance = data.currently.humidity*100;
+    icon = data.currently.icon;
+
+    document.getElementById('weather-summary').innerHTML = summ;
+    document.getElementById('temperature').innerHTML = temp;
+    document.getElementById('wind-speed').innerHTML = wind_speed + " mph";
+    document.getElementById('rain-chance').innerHTML = rain_chance + "%";
+
+    document.getElementById('image').src="./images/" + icon + ".png";
+
+
+  }
+
+
+  //sending request
+  request.send();
+
+  });
+} else {
+
+  document.write("Geolocation unavailable");
 
 }
-
-
-//sending request
-request.send();
-
-console.log("Hello");
